674170a786e31513871ab98cf2b0df61ff2c0bf3
Mlow <marcuslowsj@gmail.com>
2026-07-13 17:02:15 +0800

    added new photo and change filter to false

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-// config.js
-const CONFIG = {
-  imageFolder: 'serafufu_images/',
-  images: [
-    'serafufu_set1.png',
-    'serafufu_set2.png',
-    'serafufu_set3.png',
-    'serafufu_set4.png',
-    'serafufu_set5.png',
-  ],
-  chibiImage: 'loading/Chibi_sera.png',
-  visibleDuration: 13000,
-  invisibleDuration: 2000,
+// config.js 
+const CONFIG = { 
+  imageFolder: 'serafufu_images/', 
+  images: [ 
+    'serafufu_set1.png', 
+    'serafufu_set2.png', 
+    'serafufu_set3.png', 
+    'serafufu_set4.png', 
+    'serafufu_set5.png', 
+    'serafufu_set6.png', 
+  ], 
+  chibiImage: 'loading/Chibi_sera.png', 
+  visibleDuration: 13000, 
+  invisibleDuration: 2000, 
   enableBWFilter: false
-};
+}; 
